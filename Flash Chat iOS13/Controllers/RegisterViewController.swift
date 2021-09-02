f5121b1ef2036081cbffd3ce6de38c0d87d40856
Amir Bakhshi <amirmobili7@gmail.com>
2021-09-02 17:02:47 +0200

    Firebase added, RegisterViewController presented by a unique segue, alert message for email and password, error handling, authentication by email and password

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    // in case there was an error, here you can create an alert message
+                    let errorMsg = e.localizedDescription
+                    showAlert()
+                    
+                    // show the alert to user, message comes from firebase
+                    // the error is related either to password or email
+                    func showAlert() {
+                        let alert = UIAlertController(title: "Alert!", message: "\(errorMsg)", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: {action in
+                        }))
+                        self.present(alert, animated: true)
+                    }
+                } else {
+                    // if there is no error then a can get hold of users data
+                    // or more actions like navigate to ChatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    print(email, password)
+                }
+            }
+        }
     }
+
     
 }
